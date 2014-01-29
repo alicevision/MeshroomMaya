3fd6a642bcf256b271177793cf38fb5fb1d9506b
Nicolas Rondaud <nrondaud@gmail.com>
2014-01-29 13:26:47 +0100

    Add: new 'import' tab

--- src/mayaMVG/qt/MVGMenu.cpp
+++ src/mayaMVG/qt/MVGMenu.cpp
@@ -5,11 +5,17 @@
 #include "mayaMVG/util/MVGLog.h"
 #include "mayaMVG/util/MVGUtil.h"
 #include <maya/MQtUtil.h>
+#include <maya/MDagModifier.h>
+#include <maya/MFnCamera.h>
+#include <maya/MDagPath.h>
+
 
 using namespace mayaMVG;
 
 MVGMenu::MVGMenu(QWidget * parent) : QWidget(parent) {
 	ui.setupUi(this);
+	ui.progressBar->setVisible(false);
+	ui.cancelButton->setVisible(false);
 }
 
 MVGMenu::~MVGMenu() {
@@ -22,26 +28,59 @@ void MVGMenu::addCamera(const QString& cameraName) {
 		|| cameraName == "front" || cameraName == "side")
 		return;
 	MVGMenuItem * itemWidget = new MVGMenuItem(cameraName);
-	ui.camList->addItem(cameraName);
-	QListWidgetItem * item = ui.camList->item(ui.camList->count()-1);
-	ui.camList->setItemWidget(item, itemWidget);
+	ui.cameraList->addItem(cameraName);
+	QListWidgetItem * item = ui.cameraList->item(ui.cameraList->count()-1);
+	ui.cameraList->setItemWidget(item, itemWidget);
 	item->setSizeHint(QSize(item->sizeHint().width(), 66));
 }
 
 void MVGMenu::clear() {
-	ui.camList->clear();
+	ui.cameraList->clear();
 }
 
 void MVGMenu::selectCameras(const QList<QString>& cameraNames) {
-	for(size_t i = 0; i <  ui.camList->count(); ++i)
-		ui.camList->item(i)->setSelected(cameraNames.contains(ui.camList->item(i)->text()));
+	for(size_t i = 0; i <  ui.cameraList->count(); ++i)
+		ui.cameraList->item(i)->setSelected(cameraNames.contains(ui.cameraList->item(i)->text()));
 }
 
 // slot
-void MVGMenu::on_camList_itemSelectionChanged() {
-	QList<QListWidgetItem *> selectedItems = ui.camList->selectedItems();
+void MVGMenu::on_cameraList_itemSelectionChanged() {
+	QList<QListWidgetItem *> selectedItems = ui.cameraList->selectedItems();
 	MVGUtil::clearMayaSelection();
 	for(size_t i = 0; i < selectedItems.size(); ++i) {
 		MVGUtil::addToMayaSelection(MQtUtil::toMString(selectedItems[i]->text()));
 	}
 }
+
+// slot
+void MVGMenu::on_cameraImportButton_clicked() {
+	ui.progressBar->setVisible(true);
+	ui.cancelButton->setVisible(true);
+	MDagModifier dagModifier;
+	MStatus status;
+	MFnCamera fnCamera;
+	for(size_t i = 0; i < 10; ++i) {
+		// create camera
+		MObject cameraTransform = fnCamera.create(&status);
+		MFnDependencyNode dep(cameraTransform);
+		dep.setName("mayaMVGCam#");
+		// create image plane
+		MObject planeTransform = dagModifier.createNode("imagePlane", MObject::kNullObj, &status);
+		MFnDependencyNode fnPlane(planeTransform);
+		fnPlane.setName("imagePlane#");
+		// reparent image plane
+		MDagPath path;
+		fnCamera.getPath(path);
+		dagModifier.reparentNode(planeTransform, path.node());
+        dagModifier.doIt();
+        ui.progressBar->setValue((i+1)*10);
+	}
+	ui.progressBar->setVisible(false);
+	ui.cancelButton->setVisible(false);
+}
+
+// QString path = QFileDialog::getExistingDirectory (this, tr("Directory"), directory.path());
+// if ( path.isNull() == false )
+// {
+//     directory.setPath(path);
+// }
