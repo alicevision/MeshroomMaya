02da4a2defa98f1c9b745f7f95507147e29995e8
Fabien Castan <faca@mikrosimage.eu>
2014-03-05 10:31:50 +0100

    add log utilities

--- src/mayaMVG/util/MVGLog.h
+++ src/mayaMVG/util/MVGLog.h
@@ -15,3 +15,16 @@
 					 s << "[" << func << "] " << msg; \
 					 MGlobal::displayInfo(s.str().c_str()); \
 				   }
+#define LOG_INFO_(msg) { std::stringstream s;\
+					 s << msg; \
+					 MGlobal::displayInfo(s.str().c_str()); \
+				   }
+#define LOG_VAR(var) { std::stringstream s;\
+					 s << "-- " << #var << " = " << var; \
+					 MGlobal::displayInfo(s.str().c_str()); \
+				   }
+#define LOG_VAR2(var1, var2) { std::stringstream s;\
+					 s << "-- " << #var1 << " = " << var1; \
+					 s << ", " << #var2 << " = " << var2; \
+					 MGlobal::displayInfo(s.str().c_str()); \
+				   }
