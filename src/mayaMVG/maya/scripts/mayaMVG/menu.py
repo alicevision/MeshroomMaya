45478180d878bf2bdfc6801de0e85c67411e8d71
Nicolas Rondaud <nrondaud@gmail.com>
2014-05-07 19:37:09 +0200

    Add: menu creation using pymel

---
+++ src/mayaMVG/maya/scripts/mayaMVG/menu.py
@@ -0,0 +1,23 @@
+import pymel.core as pm
+
+
+def openMVGWindow_CB():
+    pm.MVGCmd()
+
+def mvgCreateMenu():
+	gMainWindow = pm.mel.eval('$tmpVar=$gMainWindow')
+	menuName = "mvgMenu"
+	menuLabel = "MayaMVG"
+	if(pm.menu(menuName, exists=True)):
+		pm.deleteUI(menuName)
+	if(gMainWindow != ""):
+		pm.setParent(gMainWindow)
+		menu = pm.menu(menuName, label=menuLabel, tearOff=True)
+		pm.menuItem(parent=menu, label='Open...', command=pm.Callback(openMVGWindow_CB))
+		pm.menuItem(parent=menu, divider=True)
+
+
+def mvgDeleteMenu():
+	menuName = "mvgMenu"
+	if(pm.menu(menuName, exists=True)):
+		pm.deleteUI(menuName)
